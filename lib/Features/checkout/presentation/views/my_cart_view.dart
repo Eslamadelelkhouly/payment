7ec404079b01fb2ce3a8ddf91e2f85f0f534344c
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-24 22:39:15 +0300

    add styles

--- lib/Features/checkout/presentation/views/my_cart_view.dart
+++ lib/Features/checkout/presentation/views/my_cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paymentapp/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -7,15 +8,10 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          textAlign: TextAlign.center,
-          'My Cart',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w600,
-            height: 0,
+        title: Center(
+          child: Text(
+            'My Cart',
+            style: Styles.textStyle25,
           ),
         ),
       ),
---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static TextStyle textStyle25 = const TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+    height: 0,
+  );
+  static TextStyle textstyle18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static TextStyle textstylebold18 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static TextStyle textStyle24 = TextStyle(
+    color: Colors.black,
+    fontSize: 18,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    height: 0,
+  );
+
+  static TextStyle textStyle20 = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+}
