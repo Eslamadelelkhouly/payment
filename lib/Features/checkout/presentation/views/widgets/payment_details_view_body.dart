1fcadc6824e310c786733f30f279e14cb5687446
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-27 17:02:03 +0300

    add payment method item

--- lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:paymentapp/Features/checkout/presentation/views/widgets/paymeny_method_item.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        PaymentMethodItem(),
+      ],
+    );
   }
 }
