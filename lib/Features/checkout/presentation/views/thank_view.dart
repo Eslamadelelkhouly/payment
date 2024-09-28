4e7eee0deef259b0a90066e6ba2958dfc402ef2f
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-28 23:54:38 +0300

    add thank view

---
+++ lib/Features/checkout/presentation/views/thank_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:paymentapp/Features/checkout/presentation/views/widgets/thank_view_body.dart';
+
+class ThankView extends StatelessWidget {
+  const ThankView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ThankViewBody(),
+    );
+  }
+}
