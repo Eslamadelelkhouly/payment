e00a601c9bedcf3f4799e39a78b54cac23e764bd
Eslamadelelkhouly <rodyerak@gmail.com>
2024-09-28 21:39:22 +0300

    add custom button on form credit card

--- lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:paymentapp/Features/checkout/presentation/views/widgets/custom_credit_card.dart';
 import 'package:paymentapp/Features/checkout/presentation/views/widgets/payment_method.dart';
+import 'package:paymentapp/core/widgets/custom_button.dart';
 
-class PaymentDetailsViewBody extends StatelessWidget {
+class PaymentDetailsViewBody extends StatefulWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
+  State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
+}
+
+class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          PaymentMethodListView(),
-          CustomCreditCard(),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: PaymentMethodListView(),
+        ),
+        SliverToBoxAdapter(
+          child: CustomCreditCard(),
+        ),
+        SliverFillRemaining(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                bottom: 12,
+                left: 16,
+                right: 16,
+              ),
+              child: CustomButton(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
