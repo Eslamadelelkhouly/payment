bd2d966e0663bd6abc40db3079d6fdafe474070a
Eslamadelelkhouly <rodyerak@gmail.com>
2024-10-02 00:30:06 +0300

    create services

---
+++ lib/core/utils/strip_service.dart
@@ -0,0 +1,8 @@
+import 'package:paymentapp/Features/checkout/data/models/payment_intent_input_model.dart';
+
+class StripService {
+  // PaymentIntentInputModel createPaymentIntent( PaymentIntentInputModel paymentIntentmodel)
+  // {
+
+  // }
+}
