88290a45986d89d598cddb3082ff447abcc3b47c
Eslamadelelkhouly <rodyerak@gmail.com>
2024-10-04 19:06:32 +0300

    create  createEmpheralKey function in stripe

--- lib/core/utils/strip_service.dart
+++ lib/core/utils/strip_service.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:paymentapp/Features/checkout/data/models/empheral_key_model/empheral_key_model.dart';
 import 'package:paymentapp/Features/checkout/data/models/payment_intent_input_model.dart';
 import 'package:paymentapp/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 import 'package:paymentapp/core/utils/api_keys.dart';
@@ -14,6 +15,7 @@ class StripService {
       url: 'https://api.stripe.com/v1/payment_intents',
       token: ApiKeys.secretKey,
       contentType: Headers.formUrlEncodedContentType,
+      headers: {'Authorization': "Bearer ${ApiKeys.secretKey}"},
     );
     var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
@@ -38,5 +40,23 @@ class StripService {
     await initPaymentSheet(
         paymentIntentClientSecret: paymentIntentModel.clientSecret!);
     await displayPaymentSheet();
+  }
+
+  Future<EmpheralKeyModel> createEmpheralKey(
+      {required String customerId}) async {
+    var response = await apiService.post(
+      body: {
+        'customer': customerId,
+      },
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+      contentType: Headers.formUrlEncodedContentType,
+      headers: {
+        'Authorization': "Bearer ${ApiKeys.secretKey}",
+        'Stripe-Version': '2024-06-20',
+      },
+    );
+    var empheralKey = EmpheralKeyModel.fromJson(response.data);
+    return empheralKey;
   }
 }
